c2de4b56a94258c18e4b803d71c51349e5f0e448
Steph Pla <stephpla3@gmail.com>
2018-01-23 14:18:30 -0800

    It is more or less working. Some flaws. Taking a break from the project.

--- script.js
+++ script.js
@@ -4,67 +4,68 @@ const originText = document.querySelector("#origin-text p").innerHTML;
 const resetButton = document.querySelector("#reset");
 const theTimer = document.querySelector(".timer");
 
-var timer = [0,0,0,0];
+var timer = [0, 0, 0, 0];
 var interval;
 var timerRunning = false;
 
 // Add leading zero to numbers 9 or below (purely for aesthetics):
 function leadingZero(time) {
-    if (time <= 9) {
-        time = "0" + time;
-    }
-    return time;
+  if (time <= 9) {
+    time = "0" + time;
+  }
+  return time;
 }
 
 // Run a standard minute/second/hundredths timer:
 function runTimer() {
-    let currentTime = leadingZero(timer[0]) + ":" + leadingZero(timer[1]) + ":" + leadingZero(timer[2]);
-    theTimer.innerHTML = currentTime;
-    timer[3]++;
+  let currentTime = leadingZero(timer[0]) + ":" + leadingZero(timer[1]) + ":" + leadingZero(timer[2]);
+  theTimer.innerHTML = currentTime;
+  timer[3]++;
 
-    timer[0] = Math.floor((timer[3]/100)/60);
-    timer[1] = Math.floor((timer[3]/100) - (timer[0] * 60));
-    timer[2] = Math.floor(timer[3] - (timer[1] * 100) - (timer[0] * 6000));
+  timer[0] = Math.floor((timer[3] / 100) / 60);
+  timer[1] = Math.floor((timer[3] / 100) - (timer[0] * 60));
+  timer[2] = Math.floor(timer[3] - (timer[1] * 100) - (timer[0] * 6000));
 }
 
 // Match the text entered with the provided text on the page:
 function spellCheck() {
-    let textEntered = testArea.value;
-    let originTextMatch = originText.substring(0,textEntered.length);
+  let textEntered = testArea.value;
+  let originTextMatch = originText.substring(0, textEntered.length);
 
-    if (textEntered == originText) {
-        clearInterval(interval);
-        testWrapper.style.borderColor = "#429890";
+  if (textEntered == originText) {
+    clearInterval(interval);
+    testWrapper.style.borderColor = "#429890";
+    convertTimerValue(timer);
+    //trigger method that converts timer value into number of seconds
+  } else {
+    if (textEntered == originTextMatch) {
+      testWrapper.style.borderColor = "#65CCf3";
     } else {
-        if (textEntered == originTextMatch) {
-            testWrapper.style.borderColor = "#65CCf3";
-        } else {
-            testWrapper.style.borderColor = "#E95D0F";
-        }
+      testWrapper.style.borderColor = "#E95D0F";
     }
+  }
 
 }
 
 // Start the timer:
 function start() {
-    let textEnterdLength = testArea.value.length;
-    if (textEnterdLength === 0 && !timerRunning) {
-        timerRunning = true;
-        interval = setInterval(runTimer, 10);
-    }
-    console.log(textEnterdLength);
+  let textEnterdLength = testArea.value.length;
+  if (textEnterdLength === 0 && !timerRunning) {
+    timerRunning = true;
+    interval = setInterval(runTimer, 10);
+  }
 }
 
 // Reset everything:
 function reset() {
-    clearInterval(interval);
-    interval = null;
-    timer = [0,0,0,0];
-    timerRunning = false;
+  clearInterval(interval);
+  interval = null;
+  timer = [0, 0, 0, 0];
+  timerRunning = false;
 
-    testArea.value = "";
-    theTimer.innerHTML = "00:00:00";
-    testWrapper.style.borderColor = "grey";
+  testArea.value = "";
+  theTimer.innerHTML = "00:00:00";
+  testWrapper.style.borderColor = "grey";
 }
 
 // Event listeners for keyboard input and the reset
@@ -73,29 +74,55 @@ testArea.addEventListener("keyup", spellCheck, false);
 resetButton.addEventListener("click", reset, false);
 
 //function to convert value of theTimer into seconds. New Var
+function convertTimerValue(arr) {
+  let time = 0;
+  let seconds = arr[1];
+  let milliseconds = (arr[2]/100);
+  time = seconds + milliseconds;
+  console.log(time);
+  captureLeaderboard(time);
+}
 
-
-//function that creates a collection of the different list items that make up the best times. test to see what index in the li collection the new score should have. Generate new ol to publish to page
-function captureLeaderboard() {
+//function that creates a collection of the different list items that make up the best times. test to see what index in the li collection the new score should have.
+function captureLeaderboard(newScore) {
   let scores = document.querySelectorAll("li");
-  let newScore = 1;
-  for (let i = 0; i< scores.length; i +=1) {
-    console.log(scores[i].innerHTML);
-    if (newScore < scores[i].innerHTML) {
-      console.log("new score is smaller");
-      //break;
-      //stop running loop when the new score is smaller than index
-    } else {
-      console.log("ajsbdi");
-    }
+  //let newScore = 45;
+  let newTableScores = [];
+  let bestScore = scores[0].innerHTML;
+  let bottomScore = scores[scores.length - 1].innerHTML;
+  let newIndex;
+
+  for (let index = 0; index < scores.length; index += 1) {
+    newTableScores[index] = scores[index].innerHTML;
   }
-
-
-
+  if (newScore > bottomScore) {
+    newTableScores.push(newScore);
+  } else if (newScore < bestScore) {
+    newTableScores.unshift(newScore);
+  } else if (bestScore < newScore < bottomScore) {
+    for (let i = 0; i < newTableScores.length; i++) {
+      if (newScore < newTableScores[i]) {
+        newIndex = i;
+        break;
+      }
+    }
+    newTableScores.splice(newIndex, 0, newScore);
+  }
+  generateOl(newTableScores);
 }
 
-function changeList () {
+//function to generate String of new ol if new score needs to be added, publish to page with changeList function
+function generateOl(arr) {
+  let newOlString = "";
+  for (let i = 0; i < arr.length; i += 1) {
+    newOlString += "<li>" + arr[i] + "</li>";
+  }
+  changeList(newOlString);
+}
+
+function changeList(newContent) {
   let ol = document.querySelector("ol");
+  ol.innerHTML = newContent;
 }
 
-captureLeaderboard();
+//captureLeaderboard();
